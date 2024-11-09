6697742f79f3c169d0e0144fdcddfdb55e343df0
KietCTK151 <kietvipro123456789@gmail.com>
2024-11-09 12:38:39 +0700

    MemberWindow, MemberDetailWindow, check Valid

---
+++ HotelManagement.Group5/HotelManagement/MemberDetailWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using HotelManagement.BLL.Service;
+using HotelManagement.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HotelManagement
+{
+    /// <summary>
+    /// Interaction logic for MemberDetailWindow.xaml
+    /// </summary>
+    public partial class MemberDetailWindow : Window
+    {
+        MemberService _memSer = new();
+
+        public MemberDetailWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            RoleComboBox.ItemsSource = FillRole();
+        }
+
+        private List<string> FillRole()
+        {
+            List<string> roles = new();
+            roles.Add("manager");
+            roles.Add("housekeeper");
+            return roles;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!IsValidId(IdText.Text) ||
+                !IsMatchPass(PasswordText.Text, ConfirmPasswordText.Text) ||
+                !IsValidPhone(PhoneText.Text) ||
+                !IsValidRole(RoleComboBox.SelectedValue.ToString()))
+            {
+                return;
+            }
+
+            Member mem = new Member();
+            mem.MemberId = int.Parse(IdText.Text);
+            mem.Email = EmailText.Text;
+            mem.Password = PasswordText.Text;
+            mem.Name = NameText.Text;
+            mem.Address = AddressText.Text;
+            mem.Phone = PhoneText.Text;
+            mem.Role = RoleComboBox.SelectedValue.ToString();
+            _memSer.AddMember(mem);
+        }
+
+        private bool IsValidId(string id)
+        {
+            if (!int.TryParse(id, out _))
+            {
+                MessageBox.Show("Id is not valid", "Wrong ID", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return false;
+            }
+            return true;
+        }
+
+        private bool IsValidPhone(string phone)
+        {
+            if (!double.TryParse(phone, out _))
+            {
+                MessageBox.Show("Phone is not valid", "Wrong Phone", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool IsMatchPass(string pass, string conf)
+        {
+            if (pass != conf)
+            {
+                MessageBox.Show("Password does not Match", "Wrong Password");
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool IsValidRole(string? role)
+        {
+            if (role == null)
+            {
+                MessageBox.Show("Please select one Role", "Role select", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return false;
+            }
+            return true;
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ HotelManagement.Group5/HotelManagement/MemberWindow.xaml.cs
@@ -0,0 +1,208 @@
+﻿using HotelManagement.BLL.Service;
+using HotelManagement.DAL.Entities;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HotelManagement
+{
+    /// <summary>
+    /// Interaction logic for MemberWindow.xaml
+    /// </summary>
+    public partial class MemberWindow : Window
+    {
+        Member? _selectedMember = null;
+        MemberService _memSer = new();
+
+        public Member? LoginMember { get; set; }
+
+        public MemberWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadButton_Click(object sender, RoutedEventArgs e)
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            if (LoginMember.Role == "manager")
+            {
+                CleanData();
+                MemberDataGrid.ItemsSource = _memSer.GetAllMembers();
+                SearchComboBox.ItemsSource = ListSearchOption();
+                FilterComboBox.ItemsSource = ListFilterOption();
+                return;
+            }
+            AddButton.IsEnabled = false;
+            DeleteButton.IsEnabled = false;
+        }
+
+        private List<string> ListSearchOption()
+        {
+            List<string> searchOptions = new();
+            searchOptions.Add("ID");
+            searchOptions.Add("Name");
+            return searchOptions;
+        }
+
+        private List<string> ListFilterOption()
+        {
+            List<string> filterOptions = new();
+            filterOptions.Add("manager");
+            filterOptions.Add("housekeeper");
+
+            return filterOptions;
+        }
+
+        private void CleanData()
+        {
+            IdText.Text = string.Empty;
+            EmailText.Text = string.Empty;
+            NameText.Text = string.Empty;
+            PhoneText.Text = string.Empty;
+            AddressText.Text = string.Empty;
+            RoleText.Text = string.Empty;
+            MemberDataGrid.ItemsSource = null;
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            MemberDetailWindow memberDetailWindow = new MemberDetailWindow();
+            memberDetailWindow.Show();
+        }
+
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_selectedMember == null)
+            {
+                MessageBox.Show("Please select ONE", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            _memSer.Remove(_selectedMember.MemberId);
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (SearchComboBox.SelectedValue == null)
+            {
+                MessageBox.Show("Please selected ONE", "Search FAIL", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            CleanData();
+
+            if (SearchComboBox.SelectedValue == "ID")
+            {
+                if (ListSearch("ID") == null)
+                {
+                    MessageBox.Show("this ID doesnot exist", "Search FAIL", MessageBoxButton.OK, MessageBoxImage.Information);
+                    return;
+                }
+
+                MemberDataGrid.ItemsSource = ListSearch(SearchComboBox.SelectedValue.ToString());
+                return;
+            }
+            else if (SearchComboBox.SelectedValue == "Name")
+            {
+                if (ListSearch("Name") == null)
+                {
+                    MessageBox.Show("this name does not exist", "Search FAIL", MessageBoxButton.OK, MessageBoxImage.Information);
+                    return;
+                }
+
+                MemberDataGrid.ItemsSource = ListSearch(SearchComboBox.SelectedValue.ToString());
+                return;
+            }
+        }
+
+        private List<Member>? ListSearch(string option)
+        {
+            List<Member>? list = null;
+
+            if (option == "ID")
+            {
+                list.Add(_memSer.GetMemberById(int.Parse(SearchText.Text)));
+                return list;
+            }
+            else
+            {
+                return list = _memSer.GetMembersByName(SearchText.Text);
+            }
+        }
+
+        private void FilterButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (FilterComboBox.SelectedValue == null)
+            {
+                MessageBox.Show("Please selected ONE", "Filter FAIL", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            CleanData();
+
+            if (FilterComboBox.SelectedValue == "manager")
+            {
+                MemberDataGrid.ItemsSource = ListFilter("manager");
+                return;
+            }
+            else
+            {
+                MemberDataGrid.ItemsSource = ListFilter("housekeeper");
+                return;
+            }
+        }
+
+        private List<Member>? ListFilter(string option)
+        {
+            List<Member>? list = null;
+            if (option == "manager")
+            {
+                return list = _memSer.GetMemberListByRole("manager");
+            }
+            else
+            {
+                return list = _memSer.GetMemberListByRole("housekeeper");
+            }
+        }
+
+        private void MemberDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (MemberDataGrid.SelectedItems.Count > 0)
+            {
+                _selectedMember = MemberDataGrid.SelectedItems[0] as Member;
+                FillMemberToTextBox(_selectedMember);
+            }
+        }
+
+        private void FillMemberToTextBox(Member? member)
+        {
+            IdText.Text = member.MemberId.ToString();
+            EmailText.Text = member.Email;
+            AddressText.Text = member.Address;
+            NameText.Text = member.Name;
+            PhoneText.Text = member.Phone;
+            RoleText.Text = member.Role;
+        }
+    }
+}
